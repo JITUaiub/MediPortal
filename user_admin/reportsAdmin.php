cc5e248ad359bbeb34c7049bf43ec877d7b16ae4
ghostaiub <33343723+ghostaiub@users.noreply.github.com>
2017-11-27 02:42:00 +0600

    overall users report

--- user_admin/reportsAdmin.php
+++ user_admin/reportsAdmin.php
@@ -97,11 +97,41 @@
                         </td>
                         <div align="center">
                         <td width="70%" align="center">
-                            <!--------------------------------------------------- DESIGN UI HERE-------------------------------------------------->
-
-                               
-
-                            <!------------------------------------------------------ END HERE----------------------------------------------------->
+                            <fieldset>
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <b>USER | STATISTICS</b>
+            </td>
+            <td align="right">
+                <input type="submit" value="Print"/>
+            </td>
+        </tr>
+    </table>
+</fieldset>
+<br/>
+<table width="100%" cellspacing="0" border="1" cellpadding="5">
+    <tr>
+        <th align="left">ROLE</th>
+        <th align="center" width="110">NO OF USERS</th>
+    </tr>
+    <tr>
+        <td>Admin</td>
+        <td align="center">7</td>        
+    </tr>
+    <tr>
+        <td>Executive</td>
+        <td align="center">43</td>        
+    </tr>
+    <tr>
+        <td>User</td>
+        <td align="center">296</td>        
+    </tr>
+    <tr>
+        <td align="right"><b>Total</b></td>
+        <td align="center"><b>347</b></td>        
+    </tr>
+</table>
                         </td></div>
                     </table>
                 </div>
