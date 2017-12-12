34382bb4c763c6e06284d942fcce60cd8636ffd2
Md. Nashid Kamal Jitu <nashidkamaljitu7@gmail.com>
2017-12-12 08:03:07 +0600

    ViewProfile Added
    for admin/doctor/general user

---
+++ user_admin/eConsultation/viewProfileAdmin.php
@@ -0,0 +1,223 @@
+
+<html>
+
+<head><title>Home</title></head>
+
+<body>
+    <table align="center" width="100%">
+        <tr>
+            <td>
+                <!-- Header section -->
+                <div>
+                    <table align="center" width="100%">
+                        <td width="20%">
+                            <a href="../dashboard.php"><img src="../images/pageicon.png"/></a>
+                        </td>
+                        <td width="40%">&nbsp;</td>
+                        <td width="40%">
+                            <table align="right">
+                                <td><strong>Logged in as </strong></td>
+                                <td><a href="../viewprofile.php">Bob<img src="../images/user.png"></a></td>
+                                <td><hr width="1" size="15"></td>
+                                <td><a href="../../index.html">Logout<img src="../images/logout.png"></a></td>
+                            </table> 
+                        </td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <!-- Body section -->
+               <div>
+                    <table width="100%" border="1">
+                        <!-- User Menu Section -->
+                        <td width="20%">
+                            <fieldset>
+                                <legend>
+                            <strong>Personal Information</strong></legend>
+                            <ul>
+                                <li><a href="../dashboard.php">Dashboard</a></li>
+                                <li><a href="../viewprofile.php">View Profile</a></li>
+                                <li><a href="../editprofile.php">Edit Profile</a></li>
+                                <li><a href="../changeprofilepicture.php">Change Profile Picture</a></li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset>
+                            <legend>
+                            <strong>Manage Users</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../normalUsers.php">General Users</a></li>
+                                <li><a href="../doctorUsers.php">Doctors</a></li>
+                                <li><a href="../reportedUsers.php">Reported Users</a></li>
+                            </ul>
+                        </fieldset>
+
+
+                        <fieldset>
+                            <legend>
+                            <strong>Reports</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../reportsNormalUsers.php">General Users Statistics</a></li>
+                                <li><a href="../reportsDoctorUsers.php">Doctor's Statistics</a></li>
+                                <li><a href="../reportsAdmin.php">Overall Statistics</a></li>
+                            </ul>
+                        </fieldset>
+                        
+                        
+                        <fieldset>
+                            <legend>
+                            <strong>Email</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../eConsultation/message.php">New Message</a></li>
+                                <li><a href="../eConsultation/inbox.php">Inbox</a></li>
+                                <li><a href="../eConsultation/sentitems.php">Sent items</a></li>
+                                <li><a href="../eConsultation/promoMail.php">Promotional Mail</a></li>
+                            </ul>
+                        </fieldset>
+
+
+                        <fieldset>
+                            <legend>
+                            <strong>Account</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../changepassword.html">Change Password</a></li>
+                                <li><a href="../../index.html">Logout</a></li>
+                            </ul>
+                        </fieldset>
+                        </td>
+                        <div align="center">
+                             <td width="70%" align="center">
+                                <!------ UI  -->
+                                <div>
+                                        <h1>Dread Pirate Robert</h1>
+                                            <table width="100%">
+                                                <tr>
+                                                    <td width="60%">
+                                                        <table width="100%">
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Username</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>admin</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Gender</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>Male</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Email</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>robert@example.com</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Phone</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>01700000000</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Date of Birth</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>19/9/1998</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <table width="100%">
+                                                                        <td width="10%">&nbsp;</td>
+                                                                        <td width="30%"><strong>Country</strong></td>
+                                                                        <td><strong>:</strong></td>
+                                                                        <td>Bangladesh</td>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                            <tr><td><hr/></td></tr>
+                                                            
+                                                        </table>
+                                                    </td>
+                                                    <td width="40%">
+                                                        <table align="center">
+                                                            <tr>
+                                                                <td align="center"><img src="../images/usericon.png"/></td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                             <button onclick="goBack()">Go back to conversation</button>
+
+                                                <script>
+                                                function goBack() {
+                                                    window.history.back();
+                                                }
+                                                </script> 
+                                    </div>
+                                <!-- END -->
+                            </td>
+                       </div>
+                     </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+          <td>
+                <!-- Footer section -->
+                <div>
+                    <table align="center">
+                        <tr align="center">
+                            <td>
+                            <a href="https://www.facebook.com/">
+                            <img src="../../images/facebook.png">
+                             </a>
+                         </td>
+                            <td>
+                                    <a href="https://www.twitter.com/">
+                                    <img src="../../images/twitter.png">
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                    <table align="center">
+                        <td>&copy;2017 MediPortal. All rights reserved.</td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
---
+++ user_admin/eConsultation/viewProfileUser.php
@@ -0,0 +1,132 @@
+
+<html>
+
+<head><title>Home</title></head>
+
+<body>
+    <table align="center" width="100%">
+        <tr>
+            <td>
+                <!-- Header section -->
+                <div>
+                    <table align="center" width="100%">
+                        <td width="20%">
+                            <a href="../dashboard.php"><img src="../images/pageicon.png"/></a>
+                        </td>
+                        <td width="40%">&nbsp;</td>
+                        <td width="40%">
+                            <table align="right">
+                                <td><strong>Logged in as </strong></td>
+                                <td><a href="../viewprofile.php">Bob<img src="../images/user.png"></a></td>
+                                <td><hr width="1" size="15"></td>
+                                <td><a href="../../index.html">Logout<img src="../images/logout.png"></a></td>
+                            </table> 
+                        </td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <!-- Body section -->
+               <div>
+                    <table width="100%" border="1">
+                        <!-- User Menu Section -->
+                        <td width="20%">
+                            <fieldset>
+                                <legend>
+                            <strong>Personal Information</strong></legend>
+                            <ul>
+                                <li><a href="../dashboard.php">Dashboard</a></li>
+                                <li><a href="../viewprofile.php">View Profile</a></li>
+                                <li><a href="../editprofile.php">Edit Profile</a></li>
+                                <li><a href="../changeprofilepicture.php">Change Profile Picture</a></li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset>
+                            <legend>
+                            <strong>Manage Users</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../normalUsers.php">General Users</a></li>
+                                <li><a href="../doctorUsers.php">Doctors</a></li>
+                                <li><a href="../reportedUsers.php">Reported Users</a></li>
+                            </ul>
+                        </fieldset>
+
+
+                        <fieldset>
+                            <legend>
+                            <strong>Reports</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../reportsNormalUsers.php">General Users Statistics</a></li>
+                                <li><a href="../reportsDoctorUsers.php">Doctor's Statistics</a></li>
+                                <li><a href="../reportsAdmin.php">Overall Statistics</a></li>
+                            </ul>
+                        </fieldset>
+                        
+                        
+                        <fieldset>
+                            <legend>
+                            <strong>Email</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../eConsultation/message.php">New Message</a></li>
+                                <li><a href="../eConsultation/inbox.php">Inbox</a></li>
+                                <li><a href="../eConsultation/sentitems.php">Sent items</a></li>
+                                <li><a href="../eConsultation/promoMail.php">Promotional Mail</a></li>
+                            </ul>
+                        </fieldset>
+
+
+                        <fieldset>
+                            <legend>
+                            <strong>Account</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../changepassword.html">Change Password</a></li>
+                                <li><a href="../../index.html">Logout</a></li>
+                            </ul>
+                        </fieldset>
+                        </td>
+                        <div align="center">
+                             <td width="70%" align="center">
+                                <!------ UI  -->
+                              
+                                <!-- END -->
+                            </td>
+                       </div>
+                     </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+          <td>
+                <!-- Footer section -->
+                <div>
+                    <table align="center">
+                        <tr align="center">
+                            <td>
+                            <a href="https://www.facebook.com/">
+                            <img src="../../images/facebook.png">
+                             </a>
+                         </td>
+                            <td>
+                                    <a href="https://www.twitter.com/">
+                                    <img src="../../images/twitter.png">
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                    <table align="center">
+                        <td>&copy;2017 MediPortal. All rights reserved.</td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
