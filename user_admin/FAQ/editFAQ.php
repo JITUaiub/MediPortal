3a7650712f96d23eebd2505a4455c787fa8a06a3
Md. Nashid Kamal Jitu <nashidkamaljitu7@gmail.com>
2017-12-13 12:44:46 +0600

    Manage Prev FAQ UI

---
+++ user_admin/FAQ/editFAQ.php
@@ -0,0 +1,165 @@
+<html>
+
+<head><title>Home</title></head>
+
+<body>
+	<table align="center" width="100%">
+    	<tr>
+        	<td>
+            	<!-- Header section -->
+            	<div>
+                    <table align="center" width="100%">
+                        <td width="20%">
+                            <a href="../dashboard.php"><img src="../images/pageicon.png"/></a>
+                        </td>
+                        <td width="40%">&nbsp;</td>
+                        <td width="40%">
+                            <table align="right">
+                                <td><strong>Logged in as </strong></td>
+                                <td><a href="../viewprofile.php">Bob<img src="../images/user.png"></a></td>
+                                <td><hr width="1" size="15"></td>
+                                <td><a href="../../index.html">Logout<img src="../images/logout.png"></a></td>
+                            </table> 
+                        </td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+        	<td>
+            	<!-- Body section -->
+               <div>
+                    <table width="100%" border="1">
+                        <!-- User Menu Section -->
+                        <td width="20%">
+                            <fieldset>
+                                <legend>
+                            <strong>Personal Information</strong></legend>
+                            <ul>
+                                <li><a href="../dashboard.php">Dashboard</a></li>
+                                <li><a href="../viewprofile.php">View Profile</a></li>
+                                <li><a href="../editprofile.php">Edit Profile</a></li>
+                                <li><a href="../changeprofilepicture.php">Change Profile Picture</a></li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset>
+                            <legend>
+                            <strong>Manage Users</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../normalUsers.php">General Users</a></li>
+                                <li><a href="../doctorUsers.php">Doctors</a></li>
+                                <li><a href="../reportedUsers.php">Reported Users</a></li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset>
+                            <legend>
+                            <strong>FAQ Section</strong></legend>
+                           
+                            <ul>
+                                <li><a href="newFAQ.php">New FAQ</a></li>
+                                <li><a href="requestedFAQ.php">User Requested FAQ</a></li>
+                                <li><a href="manageFAQ.php">Manage FAQ</a></li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset>
+                            <legend>
+                            <strong>Reports</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../reportsNormalUsers.php">General Users Statistics</a></li>
+                                <li><a href="../reportsDoctorUsers.php">Doctor's Statistics</a></li>
+                                <li><a href="../reportsAdmin.php">Overall Statistics</a></li>
+                            </ul>
+                        </fieldset>
+						
+						
+						<fieldset>
+                            <legend>
+                            <strong>Email</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../eConsultation/message.php">New Message</a></li>
+                                <li><a href="../eConsultation/inbox.php">Inbox</a></li>
+                                <li><a href="../eConsultation/sentitems.php">Sent items</a></li>
+                                <li><a href="../eConsultation/promoMail.php">Promotional Mail</a></li>
+                            </ul>
+                        </fieldset>
+
+
+                        <fieldset>
+                            <legend>
+                            <strong>Account</strong></legend>
+                           
+                            <ul>
+                                <li><a href="../changepassword.html">Change Password</a></li>
+                                <li><a href="../../index.html">Logout</a></li>
+                            </ul>
+                        </fieldset>
+                        </td>
+                        <div align="center">
+                       		 <td width="70%" align="center">
+                       		 	<!-- FAQ DESIGN -->
+                                <h1 align="center">Edit this FAQ</h1>
+                                    <h3 align="center">What is Mediportal?</h3>
+                                    <p align="center">Category: Mediportal<br>
+                                        Asked by: John<br>
+                                        Asked on: 22-09-2017<br>
+                                        Asked time: 10.57PM
+                                    </p>
+                                    <p align="center">
+                                        <br>bla bla bla bla bla bla bla bla bla blabla bla bla bla bla  <br>
+                                    bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla <br>
+                                    bla bla bla bla bla bla bla bla bla bla <br>
+                                    bla bla bla bla bla <br>
+                                    </p>
+                                    <br>
+
+                                    <strong>Modify this answer</strong><br><br>
+                                    <textarea>This is answer to this question. But you can modify this answer too here.</textarea>  <br><br>
+                                    <input type="submit" name="" value="Submit Answer"><br><br>
+                                    <div align="center"><button onclick="goBack()">Go Back</button></div>
+                                    <script>
+                                        function goBack()
+                                        {
+                                            window.history.back();
+                                        }
+                                    </script>
+                                <!--     FAQ  END here -->
+                    		</td>
+                    	</div>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        <tr>
+        	<td>
+            	<!-- Footer section -->
+                <div>
+                	<table align="center">
+						<tr align="center">
+							<td>
+							<a href="https://www.facebook.com/">
+							<img src="../../images/facebook.png">
+			                 </a>
+			             </td>
+							<td>
+									<a href="https://www.twitter.com/">
+									<img src="../../images/twitter.png">
+								</a>
+							</td>
+						</tr>
+					</table>
+                    <table align="center">
+                        <td>&copy;2017 MediPortal. All rights reserved.</td>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
