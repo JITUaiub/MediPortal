cf8b3fb62f180071e89e75cefd9be0cf0cbffda7
Md. Nashid Kamal Jitu <nashidkamaljitu7@gmail.com>
2017-12-25 11:54:15 +0600

    New Message

--- user_admin/eConsultation/message.php
+++ user_admin/eConsultation/message.php
@@ -1,3 +1,84 @@
+<?php
+	ini_set('mysql.connect_timeout', 300);
+	ini_set('default_socket_timeout', 300);
+
+    $recipientErr = "";
+    $bodyErr = "";
+    $err1 = false;
+    $err2 = false;
+    if(isset($_POST['submit'])){   
+    	$db_host = 'localhost';
+		$db_user = 'root';
+		$db_pass = '';
+
+		$db_name = 'mediportal_db';
+
+		$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+        $sender = "Jitu";
+        $to = $_POST['to_mail'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+        $image = null;
+
+        if(trim($subject) == "")
+            $subject = "No Subject";
+
+        if(empty($to)){
+            $recipientErr = "Enter recipient.";
+            $err1 = false;
+        }
+        else {
+        	$err1 = true;
+        	$recipientErr = "";
+        } 
+        if(empty($body)){
+            $bodyErr = "Message body cannot be empty.";
+            $err2 = false;
+        }
+        else {
+            $err2 = true; 
+            $bodyErr = "";         
+        }
+        if ($err1 == true && $err2 == true) {
+        	$sender = "Jitu";
+	        $recipient = $to;
+	       // $subject = $subject;
+	        $message = $body;
+
+	        if(getimagesize($_FILES['upload']['tmp_name']) == true) { 
+	        	$image = addslashes($_FILES['upload']['tmp_name']);
+	        	$name = addslashes($_FILES['upload']['name']);
+	        	$image = file_get_contents($image);
+	        	$image = base64_encode($image);
+	        }
+        	$query = "INSERT INTO `inbox` (`SenderName`, `RecipientName`, `Subject`, `Status`) VALUES ('{$sender}', '{$recipient}', '{$subject}', 'Unread')";
+
+			mysqli_query($connection, $query);
+
+			$query = "SELECT MAX(ChatID) from `inbox`";
+			$chatID = mysqli_fetch_assoc(mysqli_query($connection, $query));
+
+			$time = date("h:i:sa");
+			$date = date("l").' '.date("Y-m-d");
+			$query = "INSERT INTO `messages` (`ChatID`, `Body`, `attachment`, `Time`, `Date`) VALUES ({$chatID ["MAX(ChatID)"]}, '{$message}', '{$image}', '{$time}', '{$date}')";
+
+			mysqli_query($connection, $query);
+
+			//$q = "select * from messages";
+			//$arr = mysqli_query($connection, $q);
+			//while ($row = mysqli_fetch_assoc($arr)) {
+			//	echo '<img height="300" width="300" src="data:image;base64, '.$row["attachment"].'"/>';
+			//}
+
+			header("location: message.php");
+			1457exit;
+        }
+        mysqli_close($connection);
+
+	}
+?> 
+
 
 <html>
 
@@ -104,8 +185,7 @@
                         <div align="center">
                              <td width="70%" align="center" valign="top">
                                 <!------ UI  -->
-                              
-              <form action="" method="post">
+              			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
                               <h1>New Message</h1>
                               <legend align="center"><h4>Type a message here</h4></legend>
                                <fieldset>
@@ -113,17 +193,24 @@
                                              <table>
                                              <tr>
                                                  <td align="center">To</td><td>:</td>
-                                                 <td align="left"><input  type="text" name="to_mail" value="Receiver name" /></td>
+                                                 <td align="left"><input  type="text" name="to_mail" /><br>
+                                                 	<div style="color: RED">
+                                                 		<?php echo $recipientErr ?>
+                                                 	</div>
+                                                 </td>
                                              </tr>
                                              <tr><td colspan="3"><hr></td></tr>
                                              <tr>
                                                  <td align="center">Subject</td><td>:</td>
-                                                 <td><input type="text" name="subject" value="No Subject"></td>
+                                                 <td><input type="text" name="subject"></td>
                                              </tr>
                                               <tr><td colspan="3"><hr></td></tr>
                                              <tr>
                                                 <td align="center">
-                                                 <label>Body</label></td><td>:</td><td ><textarea name="body">Hello there!! How are bla bla bla bla?</textarea></td>
+                                                 <label>Body</label></td><td>:</td><td ><textarea name="body"></textarea><br>
+                                                 	<div style="color: RED">
+                                                 		<?php echo $bodyErr ?>
+                                                 	</div></td>
                                              </tr>
                                               <tr><td colspan="3"><hr></td></tr>
 
@@ -140,7 +227,7 @@
                                                   </td>
                                               </tr>
                                              </table>
-                                             <input type="submit" name="Submit" value="Send Message"><br/><br/>
+                                             <input type="submit" name="submit" value="Send Message"><br/><br/>
                                              <a href="inbox.php">Go to inbox</a>
                                          </fieldset>  
               </form>
